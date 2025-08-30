970378c344f0d8f46e39a7943775482a4809a145
Jamol Bao <jamol@live.com>
2025-08-30 16:36:54 +0800

    refine select on windows

--- src/poll/SelectPoll.cpp
+++ src/poll/SelectPoll.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, Fengping Bao <jamol@live.com>
+/* Copyright (c) 2014~2025, Fengping Bao <jamol@live.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,6 +26,185 @@
 #include <algorithm>
 
 KEV_NS_BEGIN
+
+#ifdef KUMA_OS_WIN
+class FdSet
+{
+public:
+    FdSet() {
+        size_t new_size = sizeof(ws_fd_set) + sizeof(SOCKET_FD) * (capacity_ - 1);
+        fd_set_ = (ws_fd_set*)malloc(new_size);
+        if (fd_set_) {
+            fd_set_->fd_count = 0;
+        }
+    }
+    FdSet(const FdSet& other) {
+        capacity_ = other.capacity_;
+        size_t new_size = sizeof(ws_fd_set) + sizeof(SOCKET_FD) * (capacity_ - 1);
+        fd_set_ = (ws_fd_set*)malloc(new_size);
+        if (fd_set_) {
+            fd_set_->fd_count = other.fd_set_->fd_count;
+            memcpy(&fd_set_->fd_array[0], &other.fd_set_->fd_array[0],
+                   sizeof(SOCKET_FD) * (fd_set_->fd_count));
+        }
+    }
+    FdSet(FdSet&& other) noexcept {
+        fd_set_ = other.fd_set_;
+        capacity_ = other.capacity_;
+        other.fd_set_ = nullptr;
+        other.capacity_ = 0;
+    }
+    ~FdSet() {
+        if (fd_set_) {
+            free(fd_set_);
+            fd_set_ = nullptr;
+        }
+        capacity_ = 0;
+    }
+
+    FdSet& operator=(const FdSet& other) {
+        if (this != &other) {
+            if (capacity_ != other.capacity_) {
+                if (fd_set_) {
+                    free(fd_set_);
+                }
+                capacity_ = other.capacity_;
+                size_t new_size = sizeof(ws_fd_set) + sizeof(SOCKET_FD) * (capacity_ - 1);
+                fd_set_ = (ws_fd_set*)malloc(new_size);
+            }
+            if (fd_set_) {
+                fd_set_->fd_count = other.fd_set_->fd_count;
+                memcpy(&fd_set_->fd_array[0], &other.fd_set_->fd_array[0],
+                    sizeof(SOCKET_FD) * (fd_set_->fd_count));
+            }
+        }
+        return *this;
+    }
+    FdSet& operator=(FdSet&& other) {
+        if (this != &other) {
+            if (fd_set_) {
+                free(fd_set_);
+            }
+            fd_set_ = other.fd_set_;
+            capacity_ = other.capacity_;
+            other.fd_set_ = nullptr;
+            other.capacity_ = 0;
+        }
+        return *this;
+    }
+    operator fd_set*() {
+        return reinterpret_cast<fd_set*>(fd_set_);
+    }
+
+    bool set(SOCKET_FD fd) {
+        for (u_int i = 0; i < fd_set_->fd_count; ++i) {
+            if (fd_set_->fd_array[i] == fd) {
+                return true;
+            }
+        }
+        if (reserve(fd_set_->fd_count + 1)) {
+            fd_set_->fd_array[fd_set_->fd_count++] = fd;
+            return true;
+        }
+        KM_ERRTRACE("FD_SET failed, fd=" << fd << ", fd_count=" << fd_set_->fd_count);
+        return false;
+    }
+
+    void clear(SOCKET_FD fd) {
+        for (u_int i = 0; i < fd_set_->fd_count; ++i) {
+            if (fd_set_->fd_array[i] == fd) {
+                if (i != fd_set_->fd_count - 1) {
+                    fd_set_->fd_array[i] = fd_set_->fd_array[fd_set_->fd_count - 1];
+                }
+                fd_set_->fd_count--;
+                return;
+            }
+        }
+    }
+
+    void zero() {
+        if (fd_set_) {
+            fd_set_->fd_count = 0;
+        }
+    }
+
+    bool is_set(SOCKET_FD fd) const {
+        return FD_ISSET(fd, const_cast<fd_set*>(reinterpret_cast<const fd_set*>(fd_set_)));
+    }
+private:
+    struct ws_fd_set {
+        u_int fd_count;
+        SOCKET_FD fd_array[1];
+    };
+    bool reserve(u_int n) {
+        if (n <= capacity_) {
+            return true;
+        }
+        u_int new_cap = capacity_ + 128;
+        if (n > new_cap) {
+            new_cap = n;
+        }
+        size_t new_size = sizeof(ws_fd_set) + sizeof(SOCKET_FD) * (new_cap - 1);
+        ws_fd_set* new_set = (ws_fd_set*)realloc(fd_set_, new_size);
+        if (new_set) {
+            fd_set_ = new_set;
+            capacity_ = new_cap;
+            return true;
+        }
+        return false;
+    }
+private:
+    ws_fd_set* fd_set_ { nullptr };
+    u_int      capacity_ { 64 };
+};
+#else
+class FdSet
+{
+public:
+    FdSet() {
+        FD_ZERO(&fd_set_);
+    }
+    FdSet(const FdSet& other) {
+        fd_set_ = other.fd_set_;
+    }
+    ~FdSet() {}
+
+    FdSet& operator=(const FdSet& other) {
+        if (this != &other) {
+            fd_set_ = other.fd_set_;
+        }
+        return *this;
+    }
+    operator fd_set*()
+    {
+        return &fd_set_;
+    }
+    
+    bool set(SOCKET_FD fd) {
+        if (fd < FD_SETSIZE) {
+            FD_SET(fd, &fd_set_);
+            return true;
+        }
+        KM_ERRTRACE("FD_SET failed, fd=" << fd << ", FD_SETSIZE=" << FD_SETSIZE);
+        return false;
+    }
+    
+    void clear(SOCKET_FD fd) {
+        FD_CLR(fd, &fd_set_);
+    }
+    
+    void zero() {
+        FD_ZERO(&fd_set_);
+    }
+    
+    bool is_set(SOCKET_FD fd) const {
+        return FD_ISSET(fd, const_cast<fd_set*>(&fd_set_));
+    }
+
+private:
+    fd_set fd_set_;
+};
+#endif
 
 class SelectPoll : public IOPoll
 {
@@ -56,18 +235,16 @@ private:
     NotifierPtr     notifier_ { Notifier::createNotifier() };
     PollFdVector    poll_fds_;
     
-    fd_set          read_fds_;
-    fd_set          write_fds_;
-    fd_set          except_fds_;
+    FdSet           read_fds_;
+    FdSet           write_fds_;
+    FdSet           except_fds_;
     SOCKET_FD       max_fd_;
 };
 
 SelectPoll::SelectPoll()
-: max_fd_(0)
+    : max_fd_(0)
 {
-    FD_ZERO(&read_fds_);
-    FD_ZERO(&write_fds_);
-    FD_ZERO(&except_fds_);
+
 }
 
 SelectPoll::~SelectPoll()
@@ -96,11 +273,9 @@ Result SelectPoll::registerFd(SOCKET_FD fd, KMEvent events, IOCallback cb)
     KM_INFOTRACE("SelectPoll::registerFd, fd=" << fd);
     resizePollItems(fd);
     if (INVALID_FD == poll_items_[fd].fd || -1 == poll_items_[fd].idx) {
-        PollFD pfd;
-        pfd.fd = fd;
-        pfd.events = events;
+        PollFD pfd{fd, events};
         poll_fds_.push_back(pfd);
-        poll_items_[fd].idx = int(poll_fds_.size() - 1);
+        poll_items_[fd].idx = static_cast<int>(poll_fds_.size() - 1);
     }
     poll_items_[fd].fd = fd;
     poll_items_[fd].events = events;
@@ -111,7 +286,7 @@ Result SelectPoll::registerFd(SOCKET_FD fd, KMEvent events, IOCallback cb)
 
 Result SelectPoll::unregisterFd(SOCKET_FD fd)
 {
-    auto max_fd = SOCKET_FD(poll_items_.size() - 1);
+    auto max_fd = static_cast<SOCKET_FD>(poll_items_.size() - 1);
     KM_INFOTRACE("SelectPoll::unregisterFd, fd="<<fd<<", max_fd="<<max_fd);
     if (fd < 0 || fd > max_fd) {
         KM_WARNTRACE("SelectPoll::unregisterFd, failed, max_fd=" << max_fd);
@@ -119,13 +294,13 @@ Result SelectPoll::unregisterFd(SOCKET_FD fd)
     }
     updateFdSet(fd, 0);
     int idx = poll_items_[fd].idx;
-    if(fd < max_fd) {
+    if (fd < max_fd) {
         poll_items_[fd].reset();
     } else if (fd == max_fd) {
         poll_items_.pop_back();
     }
-    int last_idx = int(poll_fds_.size() - 1);
-    if (idx > last_idx || -1 == idx) {
+    int last_idx = static_cast<int>(poll_fds_.size() - 1);
+    if (idx > last_idx || idx == -1) {
         return Result::OK;
     }
     if (idx != last_idx) {
@@ -138,21 +313,21 @@ Result SelectPoll::unregisterFd(SOCKET_FD fd)
 
 Result SelectPoll::updateFd(SOCKET_FD fd, KMEvent events)
 {
-    auto max_fd = SOCKET_FD(poll_items_.size() - 1);
-    if (fd < 0 || -1 == max_fd || fd > max_fd) {
+    auto max_fd = static_cast<SOCKET_FD>(poll_items_.size() - 1);
+    if (fd < 0 || max_fd == -1 || fd > max_fd) {
         KM_WARNTRACE("SelectPoll::updateFd, failed, fd="<<fd<<", max_fd="<<max_fd);
         return Result::INVALID_PARAM;
     }
-    if(poll_items_[fd].fd != fd) {
+    if (poll_items_[fd].fd != fd) {
         KM_WARNTRACE("SelectPoll::updateFd, failed, fd="<<fd<<", item_fd="<<poll_items_[fd].fd);
         return Result::INVALID_PARAM;
     }
     int idx = poll_items_[fd].idx;
-    if (idx < 0 || idx >= (int)poll_fds_.size()) {
+    if (idx < 0 || idx >= static_cast<int>(poll_fds_.size())) {
         KM_WARNTRACE("SelectPoll::updateFd, failed, index="<<idx);
         return Result::INVALID_STATE;
     }
-    if(poll_fds_[idx].fd != fd) {
+    if (poll_fds_[idx].fd != fd) {
         KM_WARNTRACE("SelectPoll::updateFd, failed, fd="<<fd<<", pfds_fd="<<poll_fds_[idx].fd);
         return Result::INVALID_PARAM;
     }
@@ -164,73 +339,72 @@ Result SelectPoll::updateFd(SOCKET_FD fd, KMEvent events)
 
 void SelectPoll::updateFdSet(SOCKET_FD fd, KMEvent events)
 {
-    if(events != 0) {
+    if (events != 0) {
         if (events & kEventRead) {
-            FD_SET(fd, &read_fds_);
+            read_fds_.set(fd);
         } else {
-            FD_CLR(fd, &read_fds_);
+            read_fds_.clear(fd);
         }
         if (events & kEventWrite) {
-            FD_SET(fd, &write_fds_);
+            write_fds_.set(fd);
         } else {
-            FD_CLR(fd, &write_fds_);
+            write_fds_.clear(fd);
         }
         if (events & kEventError) {
-            FD_SET(fd, &except_fds_);
+            except_fds_.set(fd);
+        } else {
+            except_fds_.clear(fd);
         }
         if (fd > max_fd_) {
             max_fd_ = fd;
         }
     } else {
-        FD_CLR(fd, &read_fds_);
-        FD_CLR(fd, &write_fds_);
-        FD_CLR(fd, &except_fds_);
-        if(max_fd_ == fd) {
-            auto it = std::max_element(poll_fds_.begin(), poll_fds_.end(), [] (PollFD& pf1, PollFD& pf2){
+        read_fds_.clear(fd);
+        write_fds_.clear(fd);
+        except_fds_.clear(fd);
+        if (max_fd_ == fd) {
+            auto it = std::max_element(poll_fds_.begin(), poll_fds_.end(), [](const PollFD& pf1, const PollFD& pf2) {
                 return pf1.fd < pf2.fd;
             });
-            max_fd_ = it != poll_fds_.end()?(*it).fd:0;
+            max_fd_ = (it != poll_fds_.end()) ? it->fd : 0;
         }
     }
 }
 
 Result SelectPoll::wait(uint32_t wait_ms)
 {
-    fd_set readfds, writefds, exceptfds;
-    memcpy(&readfds, &read_fds_, sizeof(read_fds_));
-    memcpy(&writefds, &write_fds_, sizeof(write_fds_));
-    memcpy(&exceptfds, &except_fds_, sizeof(except_fds_));
+    FdSet readfds(read_fds_), writefds(write_fds_), exceptfds(except_fds_);
     struct timeval tval { 0, 0 };
-    if(wait_ms != -1) {
-        tval.tv_sec = wait_ms/1000;
-        tval.tv_usec = (wait_ms - tval.tv_sec*1000)*1000;
+    if (wait_ms != static_cast<uint32_t>(-1)) {
+        tval.tv_sec = wait_ms / 1000;
+        tval.tv_usec = (wait_ms - tval.tv_sec * 1000) * 1000;
     }
-    int nready = ::select(static_cast<int>(max_fd_ + 1), &readfds, &writefds, &exceptfds, wait_ms == -1 ? NULL : &tval);
+    int nready = ::select(static_cast<int>(max_fd_ + 1), readfds, writefds, exceptfds, wait_ms == static_cast<uint32_t>(-1) ? nullptr : &tval);
     if (nready <= 0) {
         return Result::OK;
     }
-    // copy poll fds since event handler may unregister fd
-    PollFdVector poll_fds = poll_fds_;
-    int fds_count = int(poll_fds.size());
+    // Copy poll_fds_ since callbacks may unregister fds during iteration
+    const PollFdVector poll_fds = poll_fds_;
+    int fds_count = static_cast<int>(poll_fds.size());
     for (int i = 0; i < fds_count && nready > 0; ++i) {
         KMEvent revents = 0;
         SOCKET_FD fd = poll_fds[i].fd;
-        if(FD_ISSET(fd, &readfds)) {
+        if (readfds.is_set(fd)) {
             revents |= kEventRead;
             --nready;
         }
-        if(nready > 0 && FD_ISSET(fd, &writefds)) {
+        if (nready > 0 && writefds.is_set(fd)) {
             revents |= kEventWrite;
             --nready;
         }
-        if(nready > 0 && FD_ISSET(fd, &exceptfds)) {
+        if (nready > 0 && exceptfds.is_set(fd)) {
             revents |= kEventError;
             --nready;
         }
         if (fd < poll_items_.size()) {
             revents &= poll_items_[fd].events;
             if (revents) {
-                auto &cb = poll_items_[fd].cb;
+                auto& cb = poll_items_[fd].cb;
                 if (cb) cb(fd, revents, nullptr, 0);
             }
         }
